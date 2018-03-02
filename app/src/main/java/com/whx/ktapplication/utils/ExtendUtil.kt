29662f435ebe5e041786ef9fc3510a2d96e4131f
whx <wanghongxing02@meituan.com>
2018-03-02 16:26:16 +0800

    使用扩展函数实现的另一种ViewHolder

--- app/src/main/java/com/whx/ktapplication/utils/ExtendUtil.kt
+++ app/src/main/java/com/whx/ktapplication/utils/ExtendUtil.kt
@@ -1,6 +1,7 @@
 package com.whx.ktapplication.utils
 
 import android.content.Context
+import android.util.SparseArray
 import android.view.View
 import android.widget.Toast
 
@@ -21,6 +22,22 @@ fun Context.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
 val View.ctx: Context
     get() = context
 
+/**
+ * 通过扩展函数来来实现的另一种ViewHolder，内部使用SparseArray 来保存id 和View 的映射关系
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T : View> View.findViewOften(viewId: Int): T {
+    val viewHolder: SparseArray<View> = tag as? SparseArray<View> ?: SparseArray()
+    tag = viewHolder
+
+    var childView: View? = viewHolder.get(viewId)
+    if (childView == null) {
+        childView = findViewById(viewId)
+        viewHolder.put(viewId, childView)
+    }
+
+    return childView as T
+}
 
 fun main(args: Array<String>) {
 //    printFoo(C())
